90a144c7c0b044ef95f95145f4a2923126ef90ae
Tan Trung Vo <tantrungse@gmail.com>
2025-06-13 14:41:47 +0700

    remove test model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   get 'welcome', to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- spec/models/user_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:last_name) }
-end
